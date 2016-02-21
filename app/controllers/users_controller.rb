0de690de8d416369f468864fb260fdc41efc6101
kter <me@kter.jp>
2016-02-21 17:58:12 +0900

    do display user show page when user sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
 
     @user = User.new(user_params)    # 実装は終わっていないことに注意!
     if @user.save
-      # Handle a successful save.
+      redirect_to @user
     else
       render 'new'
     end
