f363e9c46523c365f46b7d5960a117892fcf021f
kter <me@kter.jp>
2016-02-21 21:19:02 +0900

    create login form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
   get 'signup' => 'users#new'
 
   get 'help' => 'static_pages#help'
@@ -10,6 +8,12 @@ Rails.application.routes.draw do
   get 'contact' => 'static_pages#contact'
   
   resources :users
+
+  get    'login'   => 'sessions#new'
+
+  post   'login'   => 'sessions#create'
+
+  delete 'logout'  => 'sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
