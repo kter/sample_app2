bd34618b0a222e5d6b25da15c8687ce798f3c407
kter <me@kter.jp>
2016-03-19 16:13:03 +0900

    Add routes for microposts

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,8 @@ Rails.application.routes.draw do
 
   resources :password_resets, only: [:new, :create, :edit, :update]
 
+  resources :microposts,          only: [:create, :destroy]
+  
   get    'login'   => 'sessions#new'
 
   post   'login'   => 'sessions#create'
